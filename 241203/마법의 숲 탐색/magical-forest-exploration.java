649402744d24e84639e2d3251b9873a0db3595b0
지환 <156163390+JihwanByun@users.noreply.github.com>
2024-12-03 21:03:21 +0900

    Title: 마법의 숲 탐색, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241203/마법의 숲 탐색/magical-forest-exploration.java
+++ 241203/마법의 숲 탐색/magical-forest-exploration.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Main {
     static int[] dr = {-1,0,1,0}; //북동남서
     static int[] dc = { 0,1,0,-1};
-    
+
     static int[][] forest;
     static Golem[] golems;
     static int R,C;
 
     public static void main(String[] args) {
-        
+
         Scanner sc =new Scanner(System.in);
         R = sc.nextInt();
         C = sc.nextInt();
-        forest = new int[R+1][C];
+        forest = new int[R+3][C];
 
         int k = sc.nextInt();
         int answer = 0;
@@ -26,58 +26,45 @@ public class Main {
             golems[i+1] = golem;
 
             int sum = simulate(startC-1, i+1);
-            
-            // for(int j = 0; j <= R; j++)
-            //     System.out.println(Arrays.toString(forest[j]));
+            if(sum == 0 ) continue;
+//
+//            for(int j = 0; j < forest.length; j++)
+//                System.out.println(j+ "행은: " + Arrays.toString(forest[j]));
             //갈 수 있는 제일 아래인 칸 찾기
-            // System.out.println(" ");
-            answer += sum;
+            
+            answer += sum -2  ;
         }
         System.out.println(answer);
     }
-    
-    static boolean init(int startC){
-        
-        if(forest[1][startC] != 0 || forest[2][startC] != 0){
-            return false;
-        }
-
-        return true;
-    }
 
     static int simulate(int startC, int golemNum){
-    
-        //골렘이 숲에 들어가지 못했을 때 reset
-        if(!init(startC)){
-            reset();
-            return 0;
-        }
-        //방향 설정과 이동
+
+
         int answer = move(golemNum);
-        if(golems[golemNum].block[0][0] == 0) {
+        if(golems[golemNum].block[0][0] <= 1) {
             reset();
             return 0;
         }
         return answer;
     }
 
-//방향 설정해서 이동하기
+    //방향 설정해서 이동하기
     static boolean moveDown(Golem golem){
         int empty = 0;
-        
+
         for(int i = 1 ; i < 4; i++){
-            if(golem.block[i][0] >= R || golem.block[i][1] >= C ) continue;
+            if(golem.block[i][0] + 1 >= R + 3 || golem.block[i][1] >= C ) continue;
 
             if(forest[golem.block[i][0]+1][golem.block[i][1]] == 0){
                 empty++;
             }
         }
         if(empty == 3){
-            
+
             for(int i = 0; i < 4 ; i++){
                 golem.block[i][0] += 1;
             }
-        
+
             golem.r += 1;
             return true;
         }
@@ -92,7 +79,7 @@ public class Main {
         for(int i = 0 ; i < 4 ; i++){
             if(i == 1) continue;
 
-            if(golem.block[i][1] - 1 < 0) return false; 
+            if(golem.block[i][1] - 1 < 0) return false;
             if(forest[golem.block[i][0]][golem.block[i][1] - 1] == 0){
                 empty++;
             }
@@ -110,13 +97,12 @@ public class Main {
                     golem.block[i][1] += 1;
                 }
                 golem.c += 1;
-        }
+            }
             return false;
         }
         //출구 반시계로 이동
-        golem.exitDir = (4 + golem.exitDir-1) % 4;  
-        
-        // System.out.println("exitDir left" + golem.exitDir);
+        golem.exitDir = (4 + golem.exitDir-1) % 4;
+
         return true;
     }
 
@@ -126,9 +112,9 @@ public class Main {
         int empty = 0;
         //왼쪽 이동 후 dir 바꾸고 아래쪽 이동
         for(int i = 0 ; i < 4 ; i++){
-            
+
             if(i == 2) continue;
-            if(golem.block[i][1] + 1 >= C) return false; 
+            if(golem.block[i][1] + 1 >= C) return false;
             if(forest[golem.block[i][0]][golem.block[i][1] + 1] == 0){
                 empty++;
             }
@@ -145,24 +131,22 @@ public class Main {
                 for(int i = 0 ; i < 4 ;i++){
                     golem.block[i][1] -= 1;
                 }
-            golem.c -= 1;
-            }   
+                golem.c -= 1;
+            }
             return false;
         }
         //출구 반시계로 이동
-        golem.exitDir = (4 + golem.exitDir+1) % 4;  
-        
-        // System.out.println("exitDir right " + golem.exitDir);
+        golem.exitDir = (4 + golem.exitDir+1) % 4;
+
 
         return true;
     }
-        
- //이동하기
+
+    //이동하기
     static int move(int golemNum){
 
         Golem golem = golems[golemNum];
         //남쪽 탐색
-        int dir = golem.exitDir;
         boolean canMove = true;
 
         while(canMove){
@@ -178,14 +162,14 @@ public class Main {
 
         //forest 업데이트하기
         update(golem);
-        
-        return findMaxR(golem, new boolean[R+1][C]);
+
+        return findMaxR(golem, new boolean[R+3][C]);
     }
-    
-    static int[] exit (Golem golem, int exitDir){
+
+    static int[] exit (Golem golem){
 
         if(golem.exitDir == 0){
-            golem.exit = golem.block[0];  
+            golem.exit = golem.block[0];
         } else if(golem.exitDir == 1){
             golem.exit = golem.block[1];
         } else if(golem.exitDir == 2){
@@ -197,26 +181,23 @@ public class Main {
     }
 
     public static int findMaxR(Golem golem, boolean[][] visited){
-        
-        int max = golem.block[2][0];
-        int[] exit = exit(golem, golem.exitDir);
 
-        // System.out.println("exitDir " + golem.exitDir);
+        int max = golem.block[2][0];
+        int[] exit = exit(golem);
 
         for(int i = 0 ; i< 4 ;i++){
             int movedR = golem.exit[0] + dr[i];
-            int movedC = golem.exit[1] + dc[i]; 
-            
-            if(movedR > R || movedR < 0 || movedC >= C || movedC < 0 
-            || forest[movedR][movedC] == 0 || forest[movedR][movedC] == golem.num) continue;
+            int movedC = golem.exit[1] + dc[i];
+
+            if(movedR >= R + 3 || movedR < 0 || movedC >= C || movedC < 0
+                    || forest[movedR][movedC] == 0 || forest[movedR][movedC] == golem.num) continue;
             if(visited[movedR][movedC]) continue;
             int golemContactNum = forest[movedR][movedC];
-            
+
             visited[movedR][movedC] = true;
             max = Math.max(max, findMaxR(golems[golemContactNum],visited));
             // if (golems[golemContactNum].exit[0] == movedR && golems[golemContactNum].exit[1] == movedC)
         }
-
 
         return max;
     }
@@ -227,14 +208,14 @@ public class Main {
         forest[golem.r][golem.c] = golem.num;
 
         for(int i = 0 ; i < 4; i++){
-            if(golem.block[i][0] > R || golem.block[i][1] >= C || golem.block[i][0] < 0 || golem.block[i][1] < 0 ) continue;
+            if(golem.block[i][0] >=  R + 3 || golem.block[i][1] >= C || golem.block[i][0] < 0 || golem.block[i][1] < 0 ) continue;
 
             forest[golem.block[i][0]][golem.block[i][1]] = golem.num;
         }
     }
 
     static void reset(){
-        forest = new int[R+1][C];
+        forest = new int[R+3][C];
     }
 
 }
