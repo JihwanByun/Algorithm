1dc053edf9dceec076c75c67ec09703aed362199
Jihwan Byun <156163390+JihwanByun@users.noreply.github.com>
2025-04-20 03:53:28 +0900

    [level 4] Title: 도둑질, Time: 28.54 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,40 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
-        int answer = 0;
-        
-        if(n == 3) {
-            int tmp = Math.max(money[0], money[1]);
-            answer = Math.max(tmp, money[2]);
-            return answer;
-        }
-        int candidate1 = 0; 
+           // case 1: 첫 집 포함, 마지막 집 제외
         int[] dp = new int[n];
-        dp[0] = money[0];
-        dp[1] = 0;
-        dp[2] = money[0] + money[2];
-        candidate1 = dp[2];
-        // 처음걸 포함하는 경우
-        for(int i = 3 ; i < n-1 ; i++) {
-            dp[i] = Math.max(dp[i-2] + money[i], dp[i-3] + money[i]);
-            answer = Math.max(answer, dp[i]);
-        }
         
-        //처음걸 포함하지 않는 경우
-        dp = new int[n];
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
+
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+        int case1 = dp[n - 2];
+
+        // case 2: 첫 집 제외, 마지막 집 포함
         dp[0] = 0;
         dp[1] = money[1];
-        dp[2] = money[2];
-        int candidate2 = Math.max(dp[1], dp[2]);
-        for(int i = 3 ; i < n ; i++) {
-            dp[i] = Math.max(dp[i-2] + money[i], dp[i-3] + money[i]);
-            candidate2 = Math.max(answer, dp[i]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
         }
-        
-        answer = Math.max(candidate1, candidate2);
-        
-        return answer;
+        int case2 = dp[n - 1];
+
+        return Math.max(case1, case2);
     }
 }
