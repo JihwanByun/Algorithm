3ed410570a6199009f93e28f1bbd56e44a758cad
Jihwan Byun <156163390+JihwanByun@users.noreply.github.com>
2025-04-28 17:30:17 +0900

    [level 3] Title: 입국심사, Time: 54.14 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,32 +1,26 @@
-import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
-        
-        Arrays.sort(times);
-        
-        long right = (long) times[times.length - 1] * n ;
-        long left = (long) times[0];
-        
-        long answer = Long.MAX_VALUE;
-        
-        while(right >= left){
-            
-            long mid = (right + left) / 2;
-            long people = 0;
-            
-            for(int time: times){
-                people += mid / time;
+        long left = 1;
+        long maxTime = 0;
+        for (int time : times) {
+            maxTime = Math.max(maxTime, time);
+        }
+        long right = maxTime * (long) n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long cnt = 0;
+            for (int time : times) {
+                cnt += mid / time;
             }
-            
-            if(people >= n){
+
+            if (cnt >= n) {
                 right = mid - 1;
-                answer = Math.min(answer, mid);
             } else {
                 left = mid + 1;
-            } 
+            }
         }
-        
-        return answer;
+
+        return left;
     }
 }
